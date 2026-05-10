a4632dc537b95b9df0e2032c3c5df57ee2ff4ebb
Frozzfr <saouterl@gmail.com>
2026-05-10 22:58:10 +0200

    update 1

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import {
-  createBrowserRouter,
+  HashRouter,
   RouterProvider,
 } from "react-router-dom";
 
-const router = createBrowserRouter(
+const router = createHashRouter(
   [
     {
       path: "*",
