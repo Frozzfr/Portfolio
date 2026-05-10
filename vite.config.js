0138d6867db1b6bf97b55f6108204ab96253ba7f
Frozzfr <saouterl@gmail.com>
2026-05-10 22:46:58 +0200

    mise en ligne

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true, // ou "0.0.0.0" si tu veux être explicite
-    port: 5173 // tu peux le changer si tu veux
-  }
+  base: "/Portfolio/",
 })
